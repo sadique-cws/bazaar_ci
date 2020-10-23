de703ac5bdc9b8c3b4a5cacd40bf99300f8f54ee
sadique-cws <sadique.hussain96@gmail.com>
2020-10-23 09:06:20 +0530

    add to cart

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -18,15 +18,31 @@ class User extends CI_Controller{
 
         if($item_id != null){
             $item = $this->datawork->calling("items",["id"=>$item_id]);
-            $item = $item[0];
 
-            $order = $this->datawork->calling("orders",['ordered'=>false,'user_id'=>$user->id]);
+            if(count($item) > 0){
+                $item = $item[0];
+                $order = $this->datawork->calling("orders",['ordered'=>false,'user_id'=>$user->id]);
+                if(count($order) > 0){
+                    //if exist
+                    $cond = ["ordered"=>false,"user_id"=>$user->id,"order_id"=>$order[0]->order_id,"item_id"=>$item_id];
+                    $orderitem = $this->db->where($cond)->get('orderitem')->result();
 
-            if(count($order) > 0){
-                //if exist
-            }
-            else{
-                //if not exist
+                    if(count($orderitem) > 0){
+                        $this->db->update("orderitem",["qty"=>$orderitem[0]->qty+=1],$cond);
+                    }
+                    else{
+                        $this->db->insert("orderitem",$cond);
+                    }
+                }
+                else{
+                    //if not exist
+                    $order = $this->db->insert("orders",["ordered"=>false,"user_id"=>$user->id]);
+                    echo $last_id = $this->db->insert_id();
+
+                    $orderitem = $this->db->insert("orderitem",["ordered"=>false,"user_id"=>$user->id,"order_id"=>$last_id,'item_id'=>$item_id]);
+
+                }
+               // redirect("user/cart");
             }
         }
     }
